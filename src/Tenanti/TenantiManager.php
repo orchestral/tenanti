43a59574614bd90172851c3069a490ce81a5779c
crynobone <crynobone@gmail.com>
2014-07-10 11:25:23 +0800

    Add Orchestra\Tenanti\TenantiManager::driver() method.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Tenanti/TenantiManager.php
+++ src/Tenanti/TenantiManager.php
@@ -1,16 +1,35 @@
 <?php namespace Orchestra\Tenanti;
 
+use InvalidArgumentException;
 use Illuminate\Support\Manager;
 
 class TenantiManager extends Manager
 {
     /**
+     * Create a new driver instance.
+     *
+     * @param  string  $driver
+     * @return mixed
+     * @throws \InvalidArgumentException
+     */
+    protected function createDriver($driver)
+    {
+        $config = $this->app['config']->get("orchestra/tenanti::drivers.{$driver}");
+
+        if (is_null($config)) {
+            throw new InvalidArgumentException("Driver [$driver] not supported.");
+        }
+
+        return $this->app['orchestra.tenanti.migrator']->setTenant($driver, $config);
+    }
+
+    /**
      * Get the default driver name.
      *
-     * @return string
+     * @throws \InvalidArgumentException
      */
     public function getDefaultDriver()
     {
-        throw new \InvalidArgumentException("Default driver not implemented.");
+        throw new InvalidArgumentException("Default driver not implemented.");
     }
 }
--- tests/TenantiManagerTest.php
+++ tests/TenantiManagerTest.php
@@ -1,9 +1,60 @@
 <?php namespace Orchestra\Tenanti\TestCase;
 
+use Mockery as m;
+use Illuminate\Container\Container;
 use Orchestra\Tenanti\TenantiManager;
 
 class TenantiManagerTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * Teardown the test environment.
+     */
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    /**
+     * Test Orchestra\Tenanti\TenantiManager::driver() method.
+     *
+     * @test
+     */
+    public function testDriverMethod()
+    {
+        $app = new Container;
+        $app['config'] = $config = m::mock('\Illuminate\Config\Repository');
+        $app['orchestra.tenanti.migrator'] = $migrator = m::mock('\Orchestra\Tenanti\Migrator');
+
+        $option = array('model' => 'User');
+
+        $config->shouldReceive('get')->once()->with('orchestra/tenanti::drivers.user')->andReturn($option);
+        $migrator->shouldReceive('setTenant')->once()->with('user', $option)->andReturnSelf();
+
+        $stub = new TenantiManager($app);
+
+        $resolver = $stub->driver('user');
+
+        $this->assertEquals($migrator, $resolver);
+    }
+
+    /**
+     * Test Orchestra\Tenanti\TenantiManager::driver() method
+     * when driver is not available.
+     *
+     * @test
+     * @expectedException \InvalidArgumentException
+     * @expectedMessage Driver [user] not supported.
+     */
+    public function testDriverMethodGivenDriverNotAvailable()
+    {
+        $app = new Container;
+        $app['config'] = $config = m::mock('\Illuminate\Config\Repository');
+
+        $config->shouldReceive('get')->once()->with('orchestra/tenanti::drivers.user')->andReturnNull();
+
+        with(new TenantiManager($app))->driver('user');
+    }
+
     /**
      * Test Orchestra\Tenanti\TenantiManager::getDefaultDriver()
      * is not implemented.
