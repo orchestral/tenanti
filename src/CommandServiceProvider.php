9bdf224e6664002718bd61940e44ca2606b11f44
crynobone <crynobone@gmail.com>
2015-04-14 21:18:56 +0800

    Fixed invalid SL name.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/CommandServiceProvider.php
+++ src/CommandServiceProvider.php
@@ -27,7 +27,7 @@ class CommandServiceProvider extends ServiceProvider
     protected $commands = [
         'Queued'   => 'orchestra.commands.tenanti.queue',
         'Install'  => 'orchestra.commands.tenanti.install',
-        'Make'     => 'orchestra.tenanti.tenanti.make',
+        'Make'     => 'orchestra.commands.tenanti.make',
         'Migrate'  => 'orchestra.commands.tenanti.migrate',
         'Rollback' => 'orchestra.commands.tenanti.rollback',
         'Reset'    => 'orchestra.commands.tenanti.reset',
