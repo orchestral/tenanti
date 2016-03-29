6714edc93ecb17da8429d7492cf854129d6b6e29
crynobone <crynobone@gmail.com>
2016-03-29 13:08:22 +0800

    Don't reset the database connection when $this->defaultConnection is null.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Migrator/Migrator.php
+++ src/Migrator/Migrator.php
@@ -57,7 +57,9 @@ class Migrator extends BaseMigrator
      */
     public function resetConnection()
     {
-        $this->resolver->setDefaultConnection($this->defaultConnection);
+        if (! is_null($this->defaultConnection)) {
+            $this->resolver->setDefaultConnection($this->defaultConnection);
+        }
     }
 
     /**
