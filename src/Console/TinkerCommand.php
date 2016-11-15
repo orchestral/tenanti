cf8921f59a8b5285a55b60b69196bcb46a26306c
Noorzaini Ilhami <ilhami.i906@gmail.com>
2016-11-15 18:11:04 +0800

    Add command convenience shortcuts

--- src/Console/TinkerCommand.php
+++ src/Console/TinkerCommand.php
@@ -2,6 +2,7 @@
 
 namespace Orchestra\Tenanti\Console;
 
+use Symfony\Component\Console\Exception\RuntimeException;
 use Symfony\Component\Console\Input\InputArgument;
 
 class TinkerCommand extends BaseCommand
@@ -27,8 +28,23 @@ class TinkerCommand extends BaseCommand
      */
     public function handle()
     {
-        $driver  = $this->argument('driver');
-        $id      = $this->argument('id');
+        $arg1 = $this->argument('driver');
+        $arg2 = $this->argument('id');
+
+        if (empty($arg1) && empty($arg2)) {
+            throw new RuntimeException('Not enough arguments (missing: "driver, id").');
+        } else if (empty($arg2)) {
+            $id = $arg1;
+            $driver = $this->getDriverFromConfig();
+
+            if (empty($driver)) {
+                throw new RuntimeException('Not enough arguments (missing: "driver").');
+            }
+        } else {
+            $id = $arg2;
+            $driver = $arg1;
+        }
+
         $tenanti = $this->tenant->driver($driver);
 
         $model   = $tenanti->getModel()->findOrFail($id);
@@ -45,8 +61,8 @@ class TinkerCommand extends BaseCommand
     protected function getArguments()
     {
         return [
-            ['driver', InputArgument::REQUIRED, 'Tenant driver name.'],
-            ['id', InputArgument::REQUIRED, 'The entity ID.'],
+            ['driver', InputArgument::OPTIONAL, 'Tenant driver name.'],
+            ['id', InputArgument::OPTIONAL, 'The entity ID.'],
         ];
     }
 }
