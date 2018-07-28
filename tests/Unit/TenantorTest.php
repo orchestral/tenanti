f58514eb8c1cbf348b38cebe260ba9089d720d12
Mior Muhammad Zaki <crynobone@gmail.com>
2018-07-28 08:54:58 +0800

    Improves tests.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Unit/TenantorTest.php
+++ tests/Unit/TenantorTest.php
@@ -4,6 +4,7 @@ namespace Orchestra\Tenanti\Tests\Unit;
 
 use Orchestra\Tenanti\Tenantor;
 use PHPUnit\Framework\TestCase;
+use Illuminate\Database\Eloquent\Model;
 
 class TenantorTest extends TestCase
 {
@@ -15,5 +16,23 @@ class TenantorTest extends TestCase
         $this->assertSame('company', $stub->getTenantName());
         $this->assertSame(5, $stub->getTenantKey());
         $this->assertSame('primary', $stub->getTenantConnectionName());
+        $this->assertNull($stub->getTenantModel());
+    }
+
+    /** @test */
+    public function it_can_be_initiated_from_eloquent()
+    {
+        $model = new class() extends Model {
+            protected $connection = 'primary';
+        };
+
+        $model->id = 5;
+
+        $stub = Tenantor::fromEloquent('company', $model);
+
+        $this->assertSame('company', $stub->getTenantName());
+        $this->assertSame(5, $stub->getTenantKey());
+        $this->assertSame('primary', $stub->getTenantConnectionName());
+        $this->assertSame($model, $stub->getTenantModel());
     }
 }
